26c1c1e8236d9e55e32f2c2bd794bfa2ab15b32a
박찬미 <knaem.edu@gmail.com>
2024-11-14 17:35:39 +0900

    LoginView.vue 파일 오류 수정

--- 07-02-vue-with-drf/vue-project/src/views/LogInView.vue
+++ 07-02-vue-with-drf/vue-project/src/views/LogInView.vue
@@ -15,10 +15,12 @@
 
 <script setup>
 import { ref } from 'vue'
-import { userCounterStore } from '@/stores/counter'
+import { useCounterStore } from '@/stores/counter'
 
 const username = ref(null)
 const password = ref(null)
+
+const store = useCounterStore()
 
 const logIn = function () {
   const payload = {
